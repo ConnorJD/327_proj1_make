53c02551bedba81380386be2fc8efbc114505e9e
Guest <connor.dolan.17@cnu.edu>
2019-09-10 21:57:59 -0400

    Created myfunc and created myexe again

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
+//main.cpp
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
-
-std::string func()
-{
-	return "hello";
-}
+#include <string.h>
+#include "myfunc.h"
 
 int main()
 {
